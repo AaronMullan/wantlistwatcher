bc247ba18766e531ca7e5d53cc09822d5adc477e
AaronMullan <aaroncmullan@gmail.com>
2020-02-02 07:16:44 -0800

    info section added

---
+++ src/components/info/Info.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import InfoItem from './infoitem';
+import styles from '../App.css';
+
+const Info = ({ info }) => {
+  const infoElements = info.map((item, i) => (
+    <li key={i}>
+      <InfoItem infoitem={item} />
+    </li>
+  ));
+  return (
+    <>
+      <div className={styles.Info}>
+        <h2>Info</h2>
+        <ul>
+          {infoElements}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+Info.propTypes = {
+  info: PropTypes.arrayOf(PropTypes.shape({
+    artist: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired 
+  }))
+};
--- src/components/info/infoitem.js
+++ src/components/info/infoitem.js
@@ -1,2 +1,20 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const InfoItem = ({ infoitem }) => (
+  <section>
+    <h2>{infoitem.artist}</h2>
+    <h3>{infoitem.title}</h3>
+    <p>{infoitem.price}</p>
+  </section>
+);
+
+InfoItem.propTypes = {
+  infoitem: PropTypes.shape({
+    artist: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export default InfoItem;
